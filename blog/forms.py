4a3143e1c60b6a6b7d8eb432a9c4bd2bc4080174
kertna <keerthana.k18@iiits.in>
2020-03-15 03:13:23 +0530

    profile created

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from blog.models import Post
 from django.forms import HiddenInput
+from .models import Profile
 class UserRegisterForm(UserCreationForm):
 	email=forms.EmailField()
 
@@ -11,7 +12,17 @@ class UserRegisterForm(UserCreationForm):
 		model=User
 		fields=['username','email','password1','password2']
 
+class UserUpdateForm(forms.ModelForm):
+	email=forms.EmailField()
 
+	class Meta:
+		model=User
+		fields=['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model=Profile
+		fields=['image']
 class SellForm(forms.ModelForm):
 	
 	class Meta:
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .forms import UserRegisterForm,SellForm
 from django.contrib.auth.decorators import login_required
 from blog.models import Post
+from .forms import UserUpdateForm,ProfileUpdateForm 
 def home(request):
 	return render(request,'blog/home.html')
 def about(request):
@@ -37,7 +38,23 @@ def shop(request):
 	return render(request,'blog/shop.html',context)
 @login_required
 def profile(request):
-	return render(request,'blog/profile.html')
+	if request.method== 'POST':
+		u_form=UserUpdateForm(request.POST,instance=request.user)
+		p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request,f'Your account is updated ')
+			return redirect('profile')
+	else:
+		u_form=UserUpdateForm(instance=request.user)
+		p_form=ProfileUpdateForm(instance=request.user.profile)
+	context={
+		'u_form' :u_form,
+		'p_form' :p_form
+		
+	}
+	return render(request,'blog/profile.html',context)
 
 @login_required
 def get_name(request):
